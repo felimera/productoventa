a097480cc996592a83bad57fd0515821ca64ff3a
felimera <feli041089@gmail.com>
2022-03-27 15:56:40 -0500

    Generando el JWT.

---
+++ src/main/java/com/productoventa/dto/AuthenticationResponseDto.java
@@ -0,0 +1,13 @@
+package com.productoventa.dto;
+
+public class AuthenticationResponseDto {
+    private String jwt;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
