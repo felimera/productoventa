ef71592afa5c8f8c84cb1a6f42b1cedc992ac326
felimera <feli041089@gmail.com>
2022-03-26 20:39:29 -0500

    Agregando config documentacion swagger.

--- src/main/java/com/productoventa/controller/ProductoController.java
+++ src/main/java/com/productoventa/controller/ProductoController.java
@@ -2,6 +2,9 @@ package com.productoventa.controller;
 
 import com.productoventa.dto.ProductDto;
 import com.productoventa.service.ProductInterfaceService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +15,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/productos")
+@Api(tags = "Products")
 public class ProductoController {
     @Autowired
     private ProductInterfaceService productInterfaceService;
 
     @GetMapping(path = "/findall")
+    @ApiOperation("Get all supermarket product.")
+    @ApiResponse(code = 200, message = "Ok")
     public ResponseEntity<List<ProductDto>> findAll() {
         return ResponseEntity.ok(productInterfaceService.findAll());
     }
