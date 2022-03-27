f84d3946029617fea493e3d8818789bd34bc32a2
felimera <feli041089@gmail.com>
2022-03-26 20:11:16 -0500

    Acomodando conexxion a BD.

--- src/main/java/com/productoventa/controller/ProductoController.java
+++ src/main/java/com/productoventa/controller/ProductoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/productos")
+@RequestMapping(path = "/productos")
 public class ProductoController {
     @Autowired
     private ProductInterfaceService productInterfaceService;
